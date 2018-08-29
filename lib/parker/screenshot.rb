3f765fcda96ccd4877c8bde2caf19c67f655ad27
Daniel Bogan <d@waferbaby.com>
2018-08-29 20:18:41 +1000

    Use the file basename if we don't match the pattern

--- lib/parker/screenshot.rb
+++ lib/parker/screenshot.rb
@@ -10,9 +10,15 @@ module Parker
       @path = path
 
       name_pattern = /(\d{4})(\d{2})(\d{2})(\d+).+(\.[a-z]{3})/
-      parts = File.basename(@path).match(name_pattern)
+      base_name = File.basename(@path)
 
-      @filename = parts.to_a[1..-2].join('-') << parts[-1]
+      parts = base_name.match(name_pattern)
+
+      @filename = if parts
+                    parts.to_a[1..-2].join('-') << parts[-1]
+                  else
+                    base_name
+                  end
     end
   end
 end
